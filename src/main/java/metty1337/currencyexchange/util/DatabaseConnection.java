55d07b4785dd95e50090e745cd976d0896a3ca17
Metty1337 <c137morty3@gmail.com>
2025-11-03 13:53:17 +0300

    Updated README.md

--- src/main/java/metty1337/currencyexchange/util/DatabaseConnection.java
+++ src/main/java/metty1337/currencyexchange/util/DatabaseConnection.java
@@ -12,8 +12,8 @@ import java.sql.Statement;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class DatabaseConnection {
-//    private static final String DB_URL = "jdbc:sqlite:/opt/tomcat/CurrencyExchange/data/sqlite/database.db";
-        private static final String DB_URL = "jdbc:sqlite:C:\\Users\\novik\\IdeaProjects\\CurrencyExchange\\data\\db\\sqlite\\database.db";
+    //    private static final String DB_URL = "jdbc:sqlite:/opt/tomcat/CurrencyExchange/data/sqlite/database.db";
+    private static final String DB_URL = "jdbc:sqlite:C:\\Users\\novik\\IdeaProjects\\CurrencyExchange\\data\\db\\sqlite\\database.db";
     private static final String TURN_FOREIGN_KEYS_ON = "PRAGMA foreign_keys = ON;";
 
     public static Connection getConnection() {
